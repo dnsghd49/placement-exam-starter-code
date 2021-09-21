2a2aa355a3069649b8a01508c77db5b2bb5cf81f
Aaron Lu <aaronnoh0409@gmail.com>
2021-09-21 19:25:54 -0400

    added functions for plus and mius buttons

--- assets/script.js
+++ assets/script.js
@@ -2,22 +2,40 @@
 console.log('JavaScript code has loaded!')
 
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Aaron Lu" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0      // Ginger bread
 let cc = 0      // Chocolate Chip
 let sugar = 0   // Sugar Sprinkle
+let sum = 0
 
 // Code to update name display 
 document.getElementById('credit').textContent = `Created by ${yourName}`
 
-// Event listener for clicks on the "+" button for Ginger Bread cookies
+// Ginger Bread
 document.getElementById('add-gb').addEventListener('click', function() {
-    // HINT: You can delete this console.log after you no longer need it!
-    console.log('Ginger bread + button was clicked!')
-
-    // TODO: Write the code to be run when the "+" button for "Ginger Bread" is clicked
+    document.getElementById("qty-gb").innerHTML ++
+})
+document.getElementById('minus-gb').addEventListener('click', function() {
+    document.getElementById("qty-gb").innerHTML --
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+// Chocolate Chip
+document.getElementById('add-cc').addEventListener('click', function() {
+    document.getElementById("qty-cc").innerHTML ++
+})
+
+document.getElementById('minus-cc').addEventListener('click', function() {
+    document.getElementById("qty-cc").innerHTML --
+})
+
+// Sugar Sprinkle
+document.getElementById('add-sugar').addEventListener('click', function() {
+    document.getElementById("qty-sugar").innerHTML ++
+})
+
+document.getElementById('minus-sugar').addEventListener('click', function() {
+    document.getElementById("qty-sugar").innerHTML --
+})
+
