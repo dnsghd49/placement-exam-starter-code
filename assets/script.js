d1af55c830a895c93563ef0d41c5bb89cd4ae168
Aaron Lu <aaronnoh0409@gmail.com>
2021-09-21 20:11:05 -0400

    cleaner code, working on minimum number to 0

--- assets/script.js
+++ assets/script.js
@@ -5,43 +5,43 @@ console.log('JavaScript code has loaded!')
 let yourName = "Aaron Lu" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
-let gb = 0      // Ginger bread
-let cc = 0      // Chocolate Chip
-let sugar = 0   // Sugar Sprinkle
-let sum = 0
+let gb = document.getElementById("qty-gb")      // Ginger bread
+let cc = document.getElementById("qty-cc")     // Chocolate Chip
+let sugar = document.getElementById("qty-sugar")   // Sugar Sprinkle
+let total = document.getElementById("qty-total")
 
 // Code to update name display 
 document.getElementById('credit').textContent = `Created by ${yourName}`
 
 // Ginger Bread
-document.getElementById('add-gb').addEventListener('click', function() {
-    document.getElementById("qty-gb").innerHTML ++
-    document.getElementById("qty-total").innerHTML ++
+document.getElementById('add-gb').addEventListener('click', function () {
+    gb.innerHTML++
+    total.innerHTML++
 })
-document.getElementById('minus-gb').addEventListener('click', function() {
-    document.getElementById("qty-gb").innerHTML --
-    document.getElementById("qty-total").innerHTML --
+document.getElementById('minus-gb').addEventListener('click', function () {
+    gb.innerHTML--
+    total.innerHTML--
 })
 
 // Chocolate Chip
-document.getElementById('add-cc').addEventListener('click', function() {
-    document.getElementById("qty-cc").innerHTML ++
-    document.getElementById("qty-total").innerHTML ++
+document.getElementById('add-cc').addEventListener('click', function () {
+    cc.innerHTML++
+    total.innerHTML++
 })
 
-document.getElementById('minus-cc').addEventListener('click', function() {
-    document.getElementById("qty-cc").innerHTML --
-    document.getElementById("qty-total").innerHTML --
+document.getElementById('minus-cc').addEventListener('click', function () {
+    cc.innerHTML--
+    total.innerHTML--
 })
 
 // Sugar Sprinkle
-document.getElementById('add-sugar').addEventListener('click', function() {
-    document.getElementById("qty-sugar").innerHTML ++
-    document.getElementById("qty-total").innerHTML ++
+document.getElementById('add-sugar').addEventListener('click', function () {
+    sugar.innerHTML++
+    total.innerHTML++
 })
 
-document.getElementById('minus-sugar').addEventListener('click', function() {
-    document.getElementById("qty-sugar").innerHTML --
-    document.getElementById("qty-total").innerHTML --
+document.getElementById('minus-sugar').addEventListener('click', function () {
+    sugar.innerHTML--
+    total.innerHTML--
 })
 
